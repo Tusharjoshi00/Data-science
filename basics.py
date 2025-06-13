408d1bb6813097a3a63a92ca7ef328f0fb5d63e8
Tusharjoshi00 <joshitushar1774@gmail.com>
2025-06-13 11:53:19 +0530

    new

--- basics.py
+++ basics.py
@@ -4,9 +4,13 @@
 #text =f"my age is {age}"
 #print(text)
 
-text = "my name is \"jai\""
-print(text)                    #escape chracter slash\ this is use to take two brackets
+# text = "my name is \"jai\""
+# print(text)                    #escape chracter slash\ this is use to take two brackets
 
 #from here onwards strings are completedbsdb
 # dhbhds
 # sdfsd
+
+
+def addfunction(a,b):
+    return a+b
