8ad7ed27e270c7c1515b1eb0f5572348317a1fd2
Tusharjoshi00 <joshitushar1774@gmail.com>
2025-06-26 11:00:11 +0530

    new

---
+++ st.py
@@ -0,0 +1,11 @@
+#streamlit
+#pip install streamlit
+#python lib
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.title("hello world")
